03a1fba07f87da06ad8a3d15f6a4c874568016ed
Klaus Rubrecht <klausrubrecht@gmail.com>
2025-10-07 21:59:40 +0200

    Infect with nixos-anywhere

--- infect-rabbit.sh
+++ infect-rabbit.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-# Script to install NixOS on rabbit using nixos-infect
+# Script to install NixOS on rabbit using nixos-anywhere
 # Converts Ubuntu 24.04 -> NixOS 25.05 in-place
 
 set -e
 
 HOST="root@91.98.95.99"
 
-echo "Installing NixOS on $HOST using nixos-infect..."
-echo "WARNING: This will convert the system to NixOS!"
+echo "Installing NixOS on $HOST using nixos-anywhere..."
+echo "WARNING: This will wipe the system and install NixOS!"
 read -p "Continue? (yes/no): " confirm
 if [ "$confirm" != "yes" ]; then
     echo "Aborted."
     exit 1
 fi
 
-echo "Running nixos-infect with channel 25.05..."
-ssh "$HOST" "curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | NIXOS_CHANNEL=nixos-25.05 bash -x"
+echo "Running nixos-anywhere to install NixOS 25.05..."
+nix run github:nix-community/nixos-anywhere -- --flake .#rabbit "$HOST"
 
 echo "Installation complete! The system should reboot automatically."
 echo "After reboot, you can deploy your config with: ./deploy-rabbit.sh"
