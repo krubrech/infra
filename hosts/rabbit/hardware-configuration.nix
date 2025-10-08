4e6428947b4a3f504c4738d5ed3c97e5af4a1636
Klaus Rubrecht <klausrubrecht@gmail.com>
2025-10-08 10:11:05 +0200

    Latest flake

--- hosts/rabbit/hardware-configuration.nix
+++ hosts/rabbit/hardware-configuration.nix
@@ -7,6 +7,8 @@
     efi.canTouchEfiVariables = false;
   };
 
+  boot.loader.systemd-boot.graceful = true;
+
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
