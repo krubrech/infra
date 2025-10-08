73bd6c2499dd76f1cc43df01c4c2fc939cba0f2e
Klaus Rubrecht <klausrubrecht@gmail.com>
2025-10-08 10:12:37 +0200

    No bootloader install

--- hosts/rabbit/hardware-configuration.nix
+++ hosts/rabbit/hardware-configuration.nix
@@ -2,12 +2,7 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = false;
-  };
-
-  boot.loader.systemd-boot.graceful = true;
+  boot.loader.grub.enable = false;
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
