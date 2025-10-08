63bc8ba55824923effebf5a1dd150e64133094b2
Klaus Rubrecht <klausrubrecht@gmail.com>
2025-10-08 09:01:32 +0200

    Do not use nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     disko.url   = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     # Optional project flakes (declare as many as you want; can be private via SSH)
     # inputs.my-phx-app.url = "github:YOUR_ORG/phoenix-app";
