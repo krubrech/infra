b5f8c90fd79ec96cdb205b834b0130e9d04eac51
Klaus Rubrecht <klausrubrecht@gmail.com>
2025-10-09 21:57:51 +0200

    Rabbit vm

--- hosts/rabbit/hardware-configuration.nix
+++ hosts/rabbit/hardware-configuration.nix
@@ -2,7 +2,8 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
