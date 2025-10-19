e022be0503dbbf7f2ae76dba2f0a4f2c1467986f
Klaus Rubrecht <klausrubrecht@gmail.com>
2025-10-19 15:24:25 +0200

    Allow unfree

--- modules/base.nix
+++ modules/base.nix
@@ -5,6 +5,7 @@
     networking.useNetworkd = true;
     services.resolved.enable = true;
     security.sudo.wheelNeedsPassword = false;
+    nixpkgs.config.allowUnfree = true;
     environment.systemPackages = with pkgs; [ htop git vim tmux ];
     system.stateVersion = "25.05";
   };
